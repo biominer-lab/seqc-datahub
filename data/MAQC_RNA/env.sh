e6a35fffc8dfdb0217b47cbdacabb073fbd62ec5
YJC <yjcyxky@163.com>
2022-04-04 20:09:17 +0800

    Add github action.

---
+++ data/MAQC_RNA/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# More details on https://github.com/yjcyxky/metadata-tool/blob/master/README.md
+export DB_HOST=39.104.13.190
+export DB_NAME=maqc_rna
+export DB_USER=tcoa
+export DB_TYPE=postgresql
+export DB_PORT=5432
+export METABASE_URL=http://biominer.3steps.cn/
+export DATASET_ID=2
+export DINGTALK_USERNAME=biominer
